2af27e57b72ce941419fe7de1d7873fafcf00d0e
Armans0701~ <sikder89098@gmail.com>
2023-06-21 10:53:17 +0600

    public repo

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const chefs = require("./data/chef.json");
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Chef is running");
+});
+
+app.get("/chefs", (req, res) => {
+  console.log(chefs);
+  res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedChefs = chefs.find((c) => c.id == id);
+  res.send(selectedChefs);
+});
+
+app.listen(port, () => {
+  console.log(`Dragon API is running on port: ${port}`);
+});
